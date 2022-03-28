11af35d181d5cc8bfe6202c96421854192a6dd7b
AuraSeg <ada_hack131@hackademy.mx>
2022-03-27 22:38:23 -0600

    Primer commit: django mentoria 2 completa

---
+++ skills/books/serializers.py
@@ -0,0 +1,8 @@
+#from rest_framework import serializers
+
+#from books.models import Author, Book
+
+#class AuthorSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = Author
+#        fields = '_all_'
